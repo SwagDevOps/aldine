f0eb80d0a85df429de6606ad38f7d8cc95b1332a
Dimitri Arrigoni <dimitri@arrigoni.me>
2022-08-02 20:00:56 +0200

    package(licence): add licence headers on lib files

--- lib/aldine/cli/commands/concerns/image_match.rb
+++ lib/aldine/cli/commands/concerns/image_match.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# Copyright (C) 2021-2022 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License LGPLv3+: GNU Lesser General Public License version 3 or later
+# You may obtain a copy of the License at http://www.gnu.org/licenses/lgpl.txt.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an “AS IS” BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
 require_relative '../../app'
 
 # Provide methods to match image files with a filepath without extension (``source``).
