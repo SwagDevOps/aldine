0c9c00f309507330b3eb5d24e9b43dac300dfc9d
Dimitri Arrigoni <dimitri@arrigoni.me>
2022-08-02 18:14:48 +0200

    feat(miniature_command): add position option

--- lib/aldine/cli/commands/miniature_command.rb
+++ lib/aldine/cli/commands/miniature_command.rb
@@ -17,22 +17,46 @@ class Aldine::Cli::Commands::MiniatureCommand < Aldine::Cli::Base::ErbCommand
       {
         width: 2.0,
         margin: 0.1,
+        position: self.positions.keys.fetch(0).to_s,
+      }
+    end
+
+    # @return [Hash{Symbol => String}]
+    def positions
+      {
+        right: 'r',
+        left: 'l',
       }
     end
   end
 
   # @!attribute [rw] param_width
-  #   @return [Float]
+  #   @return [Float, nil]
 
   # @!attribute [rw] param_margin
-  #   @return [Float]
+  #   @return [Float, nil]
 
-  {
-    width: 'image width',
-    margin: 'image margin'
-  }.each do |k, desc|
-    { default: self.defaults[k], attribute_name: "param_#{k}" }.then do |opts|
-      option("--#{k}", k.to_s.upcase, desc, opts) { |s| Float(s) }
+  # @!attribute [rw] param_position
+  #   @return [String, nil]
+
+  # set command options
+  begin
+    {
+      width: 'image width',
+      margin: 'image margin',
+    }.each do |k, desc|
+      { default: self.defaults[k], attribute_name: "param_#{k}" }.then do |opts|
+        option("--#{k}", k.to_s.upcase, desc, opts) { |s| Float(s) }
+      end
+    end
+
+    {
+      default: self.defaults.fetch(:position),
+      attribute_name: 'param_position',
+    }.tap do |opts|
+      option('--position', 'POSITION', 'miniature position', opts) do |s|
+        self.class.__send__(:positions).fetch(s.to_sym, s).to_s
+      end
     end
   end
 
@@ -45,6 +69,15 @@ class Aldine::Cli::Commands::MiniatureCommand < Aldine::Cli::Base::ErbCommand
   # @return [Pathname]
   def input_file
     image_match!(source)
+  end
+
+  # Get position (set from options, or default).
+  #
+  # @return [String]
+  def position
+    (self.param_position || self.defaults.fetch(:position)).then do |position|
+      self.class.__send__(:positions).fetch(position.to_sym, position).to_s
+    end
   end
 
   # Get image margin (as set from options, or default).
@@ -82,7 +115,8 @@ class Aldine::Cli::Commands::MiniatureCommand < Aldine::Cli::Base::ErbCommand
       {
         input_file: self.input_file,
         image_file: input_file.extname == '.svg' ? svg_convert(input_file) : input_file,
-        dimensions: self.dimensions
+        dimensions: self.dimensions,
+        position: self.position,
       }
     end
   end
