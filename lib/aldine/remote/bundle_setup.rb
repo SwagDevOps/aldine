14c8f77f5d2ee79c003668cc4a9c37ead279285e
Dimitri Arrigoni <dimitri@arrigoni.me>
2022-08-05 01:22:38 +0200

    feat(bundle_setup): major changes, improvements and fixes

--- lib/aldine/remote/bundle_setup.rb
+++ lib/aldine/remote/bundle_setup.rb
@@ -11,14 +11,25 @@
 require_relative '../remote'
 
 # Install bundle realted files (using symlinks).
+#
+# Sample of use:
+#
+# ```
+# Aldine::Remote::BundleSetup.new('/tmp/test_bundle', source_dir: "#{__dir__}/samples").call
+# ```
 class Aldine::Remote::BundleSetup < Aldine::Shell::Command
+  autoload(:FileUtils, 'fileutils')
   autoload(:Pathname, 'pathname')
 
   def initialize(target_dir, source_dir: nil, env: {})
     self.target_dir = Pathname.new(target_dir)
     self.source_dir = Pathname.new(source_dir || ENV.fetch('WORKDIR'))
 
-    super(arguments_builder.call, env)
+    super(arguments_builder.call, env: env)
+  end
+
+  def call
+    fs.mkdir_p(target_dir).then { super }
   end
 
   protected
@@ -58,10 +69,30 @@ class Aldine::Remote::BundleSetup < Aldine::Shell::Command
   # @return [Pathname]
   def bundle_path
     bundle_config.then do |config|
-      config.fetch('BUNDLE_PATH').then do |path|
-        path.split('/').compact.first.then do |dir|
-          source_dir.join(dir) # extract first segment and append to source dir
-        end
+      config.fetch('BUNDLE_PATH')
+            .then { |path| Pathname.new(path) }
+            .then { |path| ensure_relative_path(path, from: source_dir) }
+    end
+  end
+
+  # @return [Module<FileUtils>, Module<FileUtils::Verbose>]
+  def fs
+    silent? ? FileUtils : FileUtils::Verbose
+  end
+
+  # @param [Pathname] path
+  # @param [Pathname] from
+  #
+  # @return [Pathname]
+  def ensure_relative_path(path, from:)
+    raise "given path #{path.to_path.inspect} is not a relative path" unless path.relative?
+
+    from.join(path).relative_path_from(from).then do |relative_path|
+      raise 'relative path can not traverse the file system' if relative_path.to_path.match(%r{\.{2}/})
+
+      # extract first segment and append to ``from`` dir
+      relative_path.to_path.gsub(%r{^(\./+)+}, '').split('/').compact.first.then do |base_dir|
+        from.join(base_dir)
       end
     end
   end
